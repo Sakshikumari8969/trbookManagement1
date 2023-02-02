a410c8aa4e9506c75de048950a05f084e42beb36
Sakshikumari8969 <119596862+Sakshikumari8969@users.noreply.github.com>
2023-02-02 11:34:23 +0530

    Update joiValidation.js

--- src/validation/joiValidation.js
+++ src/validation/joiValidation.js
@@ -18,7 +18,7 @@ const bookJoi = Joi.object({
       reviews: Joi.number(),
       deletedAt: Joi.date(),
       isDeleted: Joi.boolean(),
-      releasedAt: Joi.string().regex(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/).message("please enter in this format:YYYY-MM-DD")
+      releasedAt: Joi.string().message("please enter in this format:YYYY-MM-DD")
     })
 
     // _______________________________________GET JOI-------------------------------
@@ -34,18 +34,18 @@ const bookJoi = Joi.object({
 const userJOI=Joi.object({
   title: Joi.string().trim().required().valid("Mr", "Mrs", "Miss"),
 
-  name: Joi.string().trim().required().regex(/^[a-zA-Z ]+$/).message("please enter valid name"),
+  name: Joi.string().trim().required().message("please enter valid name"),
  
   phone: Joi.string().trim().required().regex(/^[0]?[6789]\d{9}$/).message("phone is not valid"),
 
   email:Joi.string().trim().required().regex(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).message("please enter valid email"),
 
-  password: Joi.string().trim().required().min(8).max(15).regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/).message("password  should contain Min 8 character and 1 Special Symbol"),
+  password: Joi.string().trim().required().min(8).max(15).message("password  should contain Min 8 character and 1 Special Symbol"),
 
   address: Joi.object({
     street: Joi.string(),
 
-    city: Joi.string().trim().regex(/^[a-zA-Z ]+$/).message("please enter valid city name"),
+    city: Joi.string().trim().message("please enter valid city name"),
 
     pincode:Joi.string().trim().regex(/^([0-9]{4}|[0-9]{6})$/).message("please enter valid pin")
   })
@@ -58,7 +58,7 @@ const userJOI=Joi.object({
     const reviewJoi = Joi.object({
       bookId: Joi.string().required(),
 
-      reviewedBy:Joi.string().trim().regex(/^[a-zA-Z ]+$/).message("please enter valid reviewer name"),
+      reviewedBy:Joi.string().trim().message("please enter valid reviewer name"),
 
       reviewedAt:Joi.date().required(),
 
@@ -74,7 +74,7 @@ const userJOI=Joi.object({
 
     const loginJoi=Joi.object({
       email:Joi.string().trim().required().regex(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).message("please enter valid email"),
-      password: Joi.string().trim().required().min(8).max(15).regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/).message("please enter valid password"),
+      password: Joi.string().trim().required().min(8).max(15).message("please enter valid password"),
     })
 
 //=============================update book joi==============================
@@ -86,14 +86,14 @@ const userJOI=Joi.object({
 
       ISBN: Joi.string().trim().regex(/^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/).message("please enter valid ISBN"),
 
-      releasedAt: Joi.string().regex(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/).message(" please enter date in this format:YYYY-MM-DD")
+      releasedAt: Joi.string().message(" please enter date in this format:YYYY-MM-DD")
     })
 
     // ==========================update review Joi==========================
 
     const updatereviewJoi=Joi.object({
       review:Joi.string(),
-      reviewedBy:Joi.string().trim().regex(/^[a-zA-Z ]+$/).message("please enter valid reveiewer's name"),
+      reviewedBy:Joi.string().trim().message("please enter valid reveiewer's name"),
       rating:Joi.number().min(1).max(5)
     })
 
